296b1e53772c0963e078a98b67e86deb0e5b0080
huijuan.zhou <huijuan.zhou@greedyint.cn>
2016-12-16 00:19:05 +0800

    地板设置提交

--- study/demo/lufylegenddemo/js/game.js
+++ study/demo/lufylegenddemo/js/game.js
@@ -1,16 +1,180 @@
 var backLayer, loadingLayer, background, stageLayer;
 var stageSpeed = 0;
 var STAGE_STEP = 1;
-var MOVE_STEP=1;
+var MOVE_STEP = 1;
 var hero;
-var g=0.08;
+var g = 0.08;
 var imgList = {};
 var imgData = new Array(
     { name: "back", path: "./images/back.png" },
     { name: "floor0", path: "./images/floor0.png" },
     { name: "floor1", path: "./images/floor1.png" },
-    { name:"hero", path: "./images/hero.png"}
+    { name: "hero", path: "./images/hero.png" }
 );
+
+
+//背景
+function Background() {
+    base(this, LSprite, []);
+    var self = this;
+    self.bitmapData = new LBitmapData(imgList["back"]);
+    self.bitmap1 = new LBitmap(self.bitmapData);
+    self.bitmap1.scale = 2;;
+    self.addChild(self.bitmap1);
+    self.bitmap2 = new LBitmap(self.bitmapData);
+    self.bitmap2.y = self.bitmap1.getHeight();
+    self.addChild(self.bitmap2);
+    self.bitmap3 = new LBitmap(self.bitmapData);
+    self.bitmap3.y = self.bitmap1.getHeight() * 2;
+    self.addChild(self.bitmap3);
+}
+Background.prototype.run = function () {
+    var self = this;
+    self.bitmap1.y -= STAGE_STEP;
+    self.bitmap2.y -= STAGE_STEP;
+    self.bitmap3.y -= STAGE_STEP;
+    if (self.bitmap1.y < -self.bitmap1.getHeight()) {
+        self.bitmap1.y = self.bitmap2.y;
+        self.bitmap2.y = self.bitmap1.y + self.bitmap1.getHeight();
+        self.bitmap3.y = self.bitmap1.y + self.bitmap1.getHeight() * 2;
+    }
+}
+//地板
+function Floor() {
+    base(this, LSprite, []);
+    var self = this;
+    self.child = null;
+    self.hy = 0;
+    self.setView();
+}
+Floor.prototype.setView = function () { }
+Floor.prototype.onframe = function () {
+    var self = this;
+    self.y -= STAGE_STEP;
+    if (self.child) {
+        self.child.y -= STAGE_STEP;
+    }
+};
+Floor.prototype.hitRun = function () {
+
+};
+function Floor01() {
+    base(this, Floor, []);
+}
+Floor01.prototype.setView = function () {
+    var self = this;
+    self.bitmap = new LBitmap(new LBitmapData(imgList["floor0"]));
+    self.addChild(self.bitmap);
+}
+function Floor02() {
+    base(this, Floor, []);
+    var self = this;
+    self.ctrlIndex = 0;
+}
+Floor02.prototype.setView = function () {
+    var self = this;
+    self.bitmap = new LBitmap(new LBitmapData(imgList["floor1"], 0, 0, 100, 20));
+    self.addChild(self.bitmap);
+}
+Floor02.prototype.hitRun = function () {
+    var self = this;
+    self.callParent("hitRun", arguments);
+    self.ctrlIndex++;
+    if(self.ctrlIndex>=40){
+        self.parent.removeChild(this);
+    }
+    else if(self.ctrlIndex==20){
+        self.bitmap.bitmapData.setCoordinate(100,0);
+    }
+};
+function Floor03() {
+    base(this, Floor, []);
+    var self = this;
+    self.hit = false;
+    self.hy=0;
+}
+Floor03.prototype.setView = function () {
+    var self = this;
+    self.bitmap = new LBitmap(new LBitmapData(imgList["floor1"], 0, 0, 100, 20));
+    self.addChild(self.bitmap);
+}
+Floor02.prototype.hitRun = function () {
+    var self = this;
+    self.callParent("hitRun", arguments);
+    self.ctrlIndex++;
+    if(self.ctrlIndex>=40){
+        self.parent.removeChild(this);
+    }
+    else if(self.ctrlIndex==20){
+        self.bitmap.bitmapData.setCoordinate(100,0);
+    }
+};
+function Character() {
+    base(this, LSprite, []);
+    var self = this;
+    //控制左移还是右移
+    self.moveType = null;
+    self.hp = 3;//当前血量
+    self.maxHp = 3;//最大血量
+    self.hpCtrl = 0;
+    self.isJump = true;
+    self.index = 0;//动作变换的快慢
+    self.speed = 0;//下落速度
+    self._charaOld = 0;//每次下之前的y坐标
+
+    var list = LGlobal.divideCoordinate(960, 50, 1, 24);
+    var data = new LBitmapData(imgList["hero"], 0, 0, 40, 50);
+    self.animate = new LAnimation(self, data, [
+        [list[0][0]],//直立
+        [list[0][1]],//跳跃
+        [list[0][2], list[0][3], list[0][4], list[0][5], list[0][6], list[0][7], list[0][8], list[0][8], list[0][10], list[0][11], list[0][12]],//往右
+        [list[0][13], list[0][14], list[0][15], list[0][16], list[0][17], list[0][18], list[0][19], list[0][20], list[0][21], list[0][22], list[0][23]]//往左
+    ]);
+}
+Character.prototype.onframe = function () {
+    var self = this;
+    self._charaOld = self.y;
+    self.y += self.speed;
+    self.speed += g;
+    if (self.speed > 20) {
+        self.speed = 20;
+    }
+    if (self.y > LGlobal.height) {
+        self.hp = 0;
+    }
+    if (self.moveType == "left") {
+        self.x -= MOVE_STEP;
+    }
+    else if (self.moveType == "right") {
+        self.x += MOVE_STEP;
+    }
+    if (self.x < -10) {
+        self.x = -10;
+    }
+    else if (self.x > LGlobal.width - 30) {
+        self.x = LGlobal.width - 30;
+    }
+    if (self.index-- > 0) {
+        return;
+    }
+    self.index = 10;
+    self.animate.onframe();
+};
+Character.prototype.changeAction = function () {
+    var self = this;
+    if (self.moveType == "left") {
+        hero.animate.setAction(3);
+    }
+    else if (self.moveType == "right") {
+        hero.animate.setAction(2);
+    }
+    if (hero.isJump) {
+        hero.animate.setAction(1, 0);
+    }
+    else {
+        hero.animate.setAction(0, 0);
+    }
+}
 
 function main() {
     //游戏主层初始化
@@ -65,13 +229,19 @@ function gameStart(restart) {
 
     background = new Background();
     backLayer.addChild(background);
-    hero=new Character();
-    hero.x=100;
-    hero.y=140;
-    hero.hp=hero.maxHp;
+    hero = new Character();
+    hero.x = 100;
+    hero.y = 140;
+    hero.hp = hero.maxHp;
     backLayer.addChild(hero);
     stageInit();
     backLayer.addEventListener(LEvent.ENTER_FRAME, onframe);
+    if (!LGlobal.canTouch) {
+        LEvent.addEventListener(window, LKeyboardEvent.KEY_DOWN, down);
+        LEvent.addEventListener(window, LKeyboardEvent.KEY_UP, up);
+    }
+    backLayer.addEventListener(LMouseEvent.MOUSE_DOWN, mousedown);
+    backLayer.addEventListener(LMouseEvent.MOUSE_UP, mouseup);
 }
 function onframe() {
     background.run();
@@ -79,215 +249,89 @@ function onframe() {
         stageSpeed = 100;
         addStage();
     }
-    var key = null,found=false;
-    hero.jsJump=true;
+    if(!hero)return;
+    var key = null, found = false;
+    hero.isJump = true;
     for (key in stageLayer.childList) {
         var _child = stageLayer.childList[key];
-        if(_child.y<-_child.getHeight()){
+        if (_child.y < -_child.getHeight()) {
             stageLayer.removeChild(_child);
         }
-        if(!found && hero.x+30>=_child.x && hero.x<=_child.x+90 && hero.y+50>=_child.y+_child.hy && hero._charaOld+50<=_child.y+_child.hy+1){
-            hero.isJump=false;
+        if (!found && hero.x + 30 >= _child.x && hero.x <= _child.x + 90 && hero.y + 50 >= _child.y + _child.hy && hero._charaOld + 50 <= _child.y + _child.hy + 1) {
+            hero.isJump = false;
             hero.changeAction();
-            _child.child=hero;
-            hero.speed=0;
-            hero.y=_child.y-49+_child.hy;
+            _child.child = hero;
+            hero.speed = 0;
+            hero.y = _child.y - 49 + _child.hy;
             _child.hitRun();
-            found=true;
+            found = true;
         }
-        else{
-            _child.child=null;
+        else {
+            _child.child = null;
         }
         _child.onframe();
     }
-    if(hero.jsJump){
-        hero.animate.setAction(1,0);
+    if (hero.isJump) {
+        hero.animate.setAction(1, 0);
     }
-    if(hero){
+    if (hero) {
         hero.onframe();
-        if(hero.hp<=0){
+        if (hero.hp <= 0) {
             backLayer.removeChild(hero);
-            hero=null;
+            hero = null;
             gameover();
         }
     }
 }
-function gameover(){
-    
+function gameover() {
+
 }
 function stageInit() {
     stageLayer = new LSprite();
     backLayer.addChild(stageLayer);
+    var mstage;
+    mstage=new Floor01();
+    mstage.x=100;
+    mstage.y=300;
+    stageLayer.addChild(mstage);
 }
 function addStage() {
     var mstage;
-    mstage = new Floor01();
+    var index=Math.random()*2;
+    if(index<=1){
+        mstage = new Floor02();
+    }
+    else if(index<=2){
+        mstage = new Floor01();
+    }
     mstage.y = 504;
     mstage.x = Math.random() * 280;
     stageLayer.addChild(mstage);
 }
-if(!LGlobal.canTouch){
-    LEvent.addEventListener(window,LKeyboardEvent.KEY_DOWN, down);
-    LEvent.addEventListener(window,LKeyboardEvent.up, up);
-}
-    backLayer.addEventListener(LMouseEvent.MOUSE_DOWN, mousedown);
-    backLayer.addEventListener(LMouseEvent.MOUSE_up, mouseup);
-function up(){
-    hero.moveType=null;
+function up(event) {
+    if(!hero)return;
+    hero.moveType = null;
     hero.changeAction();
 }
-function down(event){
-    if(hero.moveType){
-        return;
+function down(event) {
+    if(!hero || hero.moveType)return;
+    if (event.keyCode == 37) {
+        hero.moveType = "left";
     }
-    if(event.keyCode==37){
-        hero.moveType="left";
-    }
-    else if(event.keyCode==39){
-        hero.moveType="right";
+    else if (event.keyCode == 39) {
+        hero.moveType = "right";
     }
     hero.changeAction();
 }
-function mousedown(event){
-    if(event.offsetX<=LGlobal.width*0.5){
-        down({keyCode:37});
+function mousedown(event) {
+    if (event.offsetX <= LGlobal.width * 0.5) {
+        down({ keyCode: 37 });
     }
-    else{
-        down({keyCode:39});
+    else {
+        down({ keyCode: 39 });
     }
 }
-function mouseup(event){
-    hero.moveType=null;
+function mouseup(event) {
+    hero.moveType = null;
     hero.changeAction();
-}
-
-//背景
-function Background() {
-    base(this, LSprite, []);
-    var self = this;
-    self.bitmapData = new LBitmapData(imgList["back"]);
-    self.bitmap1 = new LBitmap(self.bitmapData);
-    self.bitmap1.scale = 2;;
-    self.addChild(self.bitmap1);
-    self.bitmap2 = new LBitmap(self.bitmapData);
-    self.bitmap2.y = self.bitmap1.getHeight();
-    self.addChild(self.bitmap2);
-    self.bitmap3 = new LBitmap(self.bitmapData);
-    self.bitmap3.y = self.bitmap1.getHeight() * 2;
-    self.addChild(self.bitmap3);
-}
-Background.prototype.run = function () {
-    var self = this;
-    self.bitmap1.y -= STAGE_STEP;
-    self.bitmap2.y -= STAGE_STEP;
-    self.bitmap3.y -= STAGE_STEP;
-    if (self.bitmap1.y < -self.bitmap1.getHeight()) {
-        self.bitmap1.y = self.bitmap2.y;
-        self.bitmap2.y = self.bitmap1.y + self.bitmap1.getHeight();
-        self.bitmap3.y = self.bitmap1.y + self.bitmap1.getHeight() * 2;
-    }
-}
-//地板
-function Floor() {
-    base(this, LSprite, []);
-    var self = this;
-    self.hy = 0;
-    self.setView();
-}
-Floor.prototype.setView = function () { }
-Floor.prototype.onframe = function () {
-    var self = this;
-    self.y -= STAGE_STEP;
-    if(self.child){
-        self.child.y-=STAGE_STEP;
-    }
-};
-Floor.prototype.hitRun = function () { };
-function Floor01() {
-    base(this, Floor, []);
-}
-Floor01.prototype.setView = function () {
-    var self = this;
-    self.bitmap = new LBitmap(new LBitmapData(imgList["floor0"]));
-    self.addChild(self.bitmap);
-}
-function Floor02(){
-    base(this,Floor,[]);
-    var self=this;
-    self.ctrlIndex=0;
-}
-Floor02.prototype.setView = function () {
-    var self = this;
-    self.bitmap = new LBitmap(new LBitmapData(imgList["floor1"],0,0,100,20));
-    self.addChild(self.bitmap);
-}
-Floor2.prototype.hitRun = function () {
-    var self=this;
-    self.callParent("hitRun",arguments);
-};
-function Character(){
-    base(this,LSprite,[]);
-    var self=this;
-    //控制左移还是右移
-    self.moveType=null;
-    self.hp=3;//当前血量
-    self.maxHp=3;//最大血量
-    self.hpCtrl=0;
-    self.isJump=true;
-    self.index=0;//动作变换的快慢
-    self.speed=0;//下落速度
-    self._charaOld=0;//每次下之前的y坐标
-
-    var list=LGlobal.divideCoordinate(960,50,1,24);
-    var data=new LBitmapData(imgList["hero"],0,0,40,50);
-    self.animate=new LAnimation(self,data,[
-    [list[0][0]],//直立
-    [list[0][1]],//跳跃
-    [list[0][2],list[0][3],list[0][4],list[0][5],list[0][6],list[0][7],list[0][8],list[0][8],list[0][10],list[0][11],list[0][12]],//往右
-    [list[0][13],list[0][14],list[0][15],list[0][16],list[0][17],list[0][18],list[0][19],list[0][20],list[0][21],list[0][22],list[0][23]]//往左
-    ]);
-}
-Character.prototype.onframe=function(){
-    var self=this;
-    self._charaOld=self.y;
-    self.y+=self.speed;
-    self.speed+=g;
-    if(self.speed>20){
-        self.speed=20;
-    }
-    if(self.y>LGlobal.height){
-        self.hp=0;
-    }
-    if(self.moveType=="left"){
-        self.x-=MOVE_STEP;
-    }
-    else if(self.moveType=="right"){
-        self.x+=MOVE_STEP;
-    }
-    if(self.x<-10){
-        self.x=-10;
-    }
-    else if(self.x>LGlobal.width-30){
-        self.x=LGlobal.width-30;
-    }
-    if(self.index-- >0){
-        return;
-    }
-    self.index=10;
-    self.animate.onframe();
-};
-Character.prototype.changeAction=function(){
-    var self=this;
-    if(self.moveType=="left"){
-        hero.animate.setAction(3);
-    }
-    else if(self.moveType=="right"){
-        hero.animate.setAction(2);
-    }
-    if(hero.isJump){
-        hero.animate.setAction(1,0);
-    }
-    else{
-        hero.animate.setAction(0,0);
-    }
 }
